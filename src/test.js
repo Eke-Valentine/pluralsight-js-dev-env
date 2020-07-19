38b29a370cb972a90885abfbe3a526ae45dad240
Eke Valentine <veke717@stu.ui.edu.ng>
2020-07-19 14:09:48 +0100

    second commit

---
+++ src/test.js
@@ -0,0 +1,6 @@
+console.log(k);
+	k++;
+	print(k);
+let k=1; 
+while(k<5){
+}
---
+++ src/test.test.js
@@ -0,0 +1,10 @@
+var asset= require('assert');
+
+describe('Array', function (){
+
+	describe('#indexOf()', function() { 
+it('should return -1 when the value is nit present', function(){
+	assert.equal([1,2,3].indexOf(4),-1);
+});
+	});
+});
